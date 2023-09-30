5735e90000175eb134e07facba2088faa703d860
100472175 <100472175@alumnos.uc3m.es>
2023-10-01 01:59:00 +0200

    Añadida una manera de ejecutar en avignon fácil

---
+++ all_avignon.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sbatch cmake.sh
+sleep 2
+sbatch make.sh
+sleep 2
+job_id_fluid=$(sbatch fluid.sh | awk '{print $4}')
+echo "Submitted batch job $job_id_fluid"
+sleep 3
+tail -f slurm-$job_id_fluid.out &
