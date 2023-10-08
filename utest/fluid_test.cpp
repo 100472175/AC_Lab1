0b3114c08784cd569766a70e0766094519c03710
CLopMan <cesarlopman@gmail.com>
2023-10-08 19:39:58 +0200

    google test

---
+++ utest/fluid_test.cpp
@@ -0,0 +1,19 @@
+//
+// Created by cesar on 08/10/23.
+//
+
+#include "gtest/gtest.h"
+
+TEST(String_not_equal, not_equal) {
+    EXPECT_STRNE("HELLO", "WORLD");
+}
+
+TEST(Arithmetic, equal) {
+    EXPECT_EQ(7*6, 42);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+    return 0;
+}
