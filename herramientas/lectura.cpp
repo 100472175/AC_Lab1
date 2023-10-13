28d823b81d4534a56b65ed134b89110f098b91d4
100472175 <100472175@alumnos.uc3m.es>
2023-10-13 13:42:54 +0200

    Mismo formato de lectura que traza

--- herramientas/lectura.cpp
+++ herramientas/lectura.cpp
@@ -1,107 +1,47 @@
-#include <iostream> 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdint>
 
-/*
- | cte | Descripción            | valor    |
- |-----|------------------------|----------|
- | r   | Multiplicador de radio | 1.695    |
- | ρ   | Densidad de fluido     | 103      |
- | ps  | Presión de rigidez     | 3.0      | 
- | sc  | Colisiones de rigidez  | 3 · 104  |
- | dv  | Amortiguamiento        | 128.0    |
- | μ   | Viscosidad             | 0.4      |
- | dp  | Tamaño de partícula    | 2 · 10−4 |
- | ∆t  | Paso de tiempo         | 10−3     |
+struct Particle {
+    float posx, posy, posz;
+    float hvx, hvy, hvz;
+    float velx, vely, vlez;
+};
 
-*/
-typedef struct _particle {
-    float px;
-    float py;
-    float pz;
-    float hvx;
-    float hvy;
-    float hvz;
-    float vx;
-    float vy;
-    float vz;
-} particle;
+int main(int argc, char** argv) {
+  std::ifstream file(argv[1], std::ios::binary);
 
-int main() {
-  FILE* small = fopen("final.fld", "r");
+  if (!file) {
+    std::cerr << "Error al abrir el archivo." << std::endl;
+    return 1;
+  }
+
   float ppm;
   int32_t np;
-  int n_bytes = 0;
-  n_bytes = fread(&ppm, 4, 1, small);
-  if (n_bytes != 1) {
-    printf("ERROR: ppm nor read correctly\n");
-    return -1;
+
+  // Leer la cabecera
+  file.read(reinterpret_cast<char*>(&ppm), sizeof(float));
+  file.read(reinterpret_cast<char*>(&np), sizeof(int32_t));
+
+  std::cout << "FILE: " << argv[1] << std::endl;
+  std::cout << "==============\n";
+  std::cout << "particulas por metro: " << ppm << std::endl;
+  std::cout << "numero de particulas: " << np << "\n==============\n";
+
+  for (int32_t i = 0; i < np; ++i) {
+    Particle p;
+    file.read(reinterpret_cast<char*>(&p), sizeof(Particle));
+
+    std::cout << "***********" << std::endl;
+    std::cout << "Particula: " << i << std::endl;
+    std::cout << "Position (x, y, z): " << p.posx << ", " << p.posy << ", " << p.posz << std::endl;
+    std::cout << "hv (x, y, z): " << p.hvx << ", " << p.hvy << ", " << p.hvz << std::endl;
+    std::cout << "Velocity (x, y, z): " << p.velx << ", " << p.vely << ", " << p.vlez << std::endl;
+    std::cout << "***********" << std::endl;
   }
-  printf("===================\n");
-  printf("ppm: = %f\n", ppm);
-  n_bytes = fread(&np, 4, 1, small);
 
-  if (n_bytes != 1) {
-    printf("ERROR: ppm nor read correctly\n");
-    return -1;
-  }
+  file.close();
 
-  printf("np = %i\n", np);
-  printf("===================\n");
-
-  for (int i = 0; i < np; ++i) {
-    float buffer[9];
-    n_bytes = fread(buffer, 4, 9, small);
-    if (n_bytes < 9) {
-      printf("ERROR: datos de partícula %i mal leídos", i);
-      return -1;
-    }
-    printf("***\n");
-    printf("particle %i:\n  px = %f\n  py = %f\n  pz = %f\n  hx = %f\n  hy = %f\n  hz = %f\n  vx = %f\n  vy = %f\n  vz = %f\n", i, buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6], buffer[7], buffer[8]);
-    printf("***\n");
-    return 0;
-  }
-  n_bytes = fread(&ppm, 1, 1, small);
-  if (n_bytes > 0) printf("queda archivo sin leer\n");
-
-    return 0;
-}
-
-int main_() {
-    FILE* small = fopen("out/small-1.fld", "r");
-    float ppm;
-    int32_t np;
-    int n_bytes = 0;
-    n_bytes = fread(&ppm, 4, 1, small);
-    if (n_bytes != 1) {
-    printf("ERROR: ppm nor read correctly\n");
-    return -1;
-    }
-    printf("===================\n");
-    printf("ppm: = %f\n", ppm);
-    n_bytes = fread(&np, 4, 1, small);
-
-    if (n_bytes != 1) {
-    printf("ERROR: ppm nor read correctly\n");
-    return -1;
-    }
-
-    printf("np = %i\n", np);
-    printf("===================\n");
-
-    for (int i = 0; i < np; ++i) {
-    float buffer[9];
-    n_bytes = fread(buffer, 4, 9, small);
-    if (n_bytes < 9) {
-      printf("ERROR: datos de partícula %i mal leídos", i);
-      return -1;
-    }
-    printf("***\n");
-    printf("particle %i:\n  px = %f\n  py = %f\n  pz = %f\n  hx = %f\n  hy = %f\n  hz = %f\n  vx = %f\n  vy = %f\n  vz = %f\n", i, buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6], buffer[7], buffer[8]);
-    printf("***\n");
-    }
-    n_bytes = fread(&ppm, 1, 1, small);
-    if (n_bytes > 0) printf("queda archivo sin leer\n");
-
-
-
-    return 0;
+  return 0;
 }
