f5ab47769b88eea6037df7fc6b0b7b690a21592d
100472175 <100472175@alumnos.uc3m.es>
2023-10-20 15:49:50 +0200

    Vector sin templates

--- sim/vector_3d.hpp
+++ sim/vector_3d.hpp
@@ -6,67 +6,80 @@
 #define AC_LAB1_VECTOR_3D_H
 #include <cmath>
 
-template<typename T>
+constexpr double cuadrado(double numero) {
+  return numero * numero;
+}
+
+template <typename T>
 struct Vector3d {
     T x, y, z;
     constexpr Vector3d(T x, T y, T z) : x(x), y(y), z(z){};
 
     void set_values(T a, T b, T c) {
-        x = a;
-        y = b;
-        z = c;
-    }
-    Vector3d<float> to_float() {
-        return {(float)x, (float)y , (float)z};
-    }
-    Vector3d<double> to_double() {
-        return {(double)x, (double)y, (double)z};
+      x = a;
+      y = b;
+      z = c;
     }
 
-    static double sq_distancia(Vector3d pos1, Vector3d pos2){
-        return pow(pos1.x - pos2.x, 2) + pow(pos1.y - pos2.y, 2) + pow(pos1.z - pos2.z, 2);
+    Vector3d<float> to_float() { return {(float) x, (float) y, (float) z}; }
+
+    Vector3d<double> to_double() { return {(double) x, (double) y, (double) z}; }
+
+    static double sq_distancia(Vector3d pos1, Vector3d pos2) {
+      return cuadrado(pos1.x - pos2.x) + cuadrado(pos1.y - pos2.y) + cuadrado(pos1.z - pos2.z);
     }
+
     static double distancia(Vector3d pos1, Vector3d pos2) {
-        return sqrt(pow(pos1.x - pos2.x, 2) + pow(pos1.y - pos2.y, 2) + pow(pos1.z - pos2.z, 2));
+      return sqrt(pow(pos1.x - pos2.x, 2) + pow(pos1.y - pos2.y, 2) + pow(pos1.z - pos2.z, 2));
     }
-    // Para sumar dos vectores
-    void operator+=(Vector3d<T> const & v) {
-        x += v.x;
-        y += v.y;
-        z += v.z;
-    }
-    Vector3d<T> operator+(Vector3d<T> const & other) const {
-        return {this->x + other.x, this->y + other.y, this->z + other.z};
-    }
-    // Para restar dos vectores
-    void operator-=(Vector3d const & v) {
-        x -= v.x;
-        y -= v.y;
-        z -= v.z;
-    }
-    Vector3d<T> operator-(Vector3d const & other) const {
-        return {this->x - other.x, this->y - other.y, this->z - other.z};
-    }
-    // Para multiplicar todos los elementos por un escalar
-    void operator*=(double const & scalar) {
-        x *= scalar;
-        y *= scalar;
-        z *= scalar;
-    }
-    Vector3d<double> operator*(double const & scalar) const {
-        return {this->x * scalar, this->y * scalar, this->z * scalar};
 
+    // Para sumar dos vectores
+    Vector3d operator+=(Vector3d<T> const & v) {
+      x += v.x;
+      y += v.y;
+      z += v.z;
+      return *this;
     }
+
+    Vector3d operator+(Vector3d<T> const & other) const {
+      return {this->x + other.x, this->y + other.y, this->z + other.z};
+    }
+
+    // Para restar dos vectores
+    Vector3d operator-=(Vector3d const & v) {
+      x -= v.x;
+      y -= v.y;
+      z -= v.z;
+      return *this;
+    }
+
+    Vector3d operator-(Vector3d const & other) const {
+      return {this->x - other.x, this->y - other.y, this->z - other.z};
+    }
+
+    // Para multiplicar todos los elementos por un escalar
+    Vector3d operator*=(double const & scalar) {
+      x *= scalar;
+      y *= scalar;
+      z *= scalar;
+      return *this;
+    }
+
+    Vector3d operator*(T const scalar) const {
+      return {this->x * scalar, this->y * scalar, this->z * scalar};
+    }
+
     // Para dividir todos los elementos por un escalar
-    void operator/=(double const & scalar) {
-        x /= scalar;
-        y /= scalar;
-        z /= scalar;
+    Vector3d operator/=(double const & scalar) {
+      x /= scalar;
+      y /= scalar;
+      z /= scalar;
+      return *this;
     }
+
     // Para dividir todos los elementos por un vector
     Vector3d<double> operator/(Vector3d<T> const & other) const {
-        return {this->x / (double)other.x, this->y / (double)other.y, this->z / (double)other.z};
-
+      return {this->x / (double) other.x, this->y / (double) other.y, this->z / (double) other.z};
     }
 };
 
