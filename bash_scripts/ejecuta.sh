d55451fa1cce8cd93d3d65049915c6864d6f49ec
edu <100472175@alumnos.uc3m.es>
2023-11-17 17:13:38 +0100

    Nuevo ejecuta con to'

--- bash_scripts/ejecuta.sh
+++ bash_scripts/ejecuta.sh
@@ -4,4 +4,11 @@
 module avail
 module load gcc/12.1.0
 echo "*** BASE ***"
-perf stat  build/fluid/fluid 1000 fluid-2023/in/large.fld out-base.fld
+perf stat build/fluid/fluid 1000 fluid-2023/in/large.fld kk.fld
+perf stat -a -e 'power/energy-cores/, power/energy-gpu/, power/energy-pkg/, power/energy-ram/' build/fluid/fluid 1000 fluid-2023/in/large.fld kk.fld
+
+
+echo "*** OPTIMIZED ***"
+build/fluid/fluid 5 fluid-2023/in/large.fld 5.fld
+echo "*** DIFF CORRECTO SI NO HAY NADA DEBAJO ***"
+diff 5.fld out/large-5.fld
